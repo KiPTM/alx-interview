e1552abe65f17530bab8544ea97642905fb9843c
KiPTM <mathewtuwei@gmail.com>
2024-08-28 09:56:39 +0000

    Update

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Triangle Triangle"""
+
+
+def pascal_triangle(n):
+    """Triangle"""
+    if n <= 0:
+        return []
+    pasc = [[1]]
+    for row_number in range(1, n):
+        row = [1]
+        for j in range(1, row_number):
+            element = pasc[row_number - 1][j - 1] + pas[row_number - 1][j]
+            row.append(element)
+        row.append(1)
+        pasc.append(row)
+
+    return pasc
